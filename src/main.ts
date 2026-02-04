ee6ad2a0eb95d1057a7c05fb03d96484e732a735
linasvard <linajsvard@hotmail.com>
2026-02-04 11:20:24 +0100

    ✨ toggle inmatning

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,20 @@
 
 import './style.scss'
 
+const incomeRadioBtn = document.querySelector('input[type="radio"].income');
+const expenseRadioBtn = document.querySelector('input[type="radio"].expense');
+
+incomeRadioBtn?.addEventListener('change', toggleIncomeOrExpense);
+expenseRadioBtn?.addEventListener('change', toggleIncomeOrExpense);
+
 function toggleIncomeOrExpense(e) {
-  const isIncome = e.target.value === 'income';
+  const selectedInput = e.target.value;
+
+  if(selectedInput == 'income') {
+    document.querySelector('#income')?.classList.remove('hidden');
+    document.querySelector('#expense')?.classList.add('hidden');
+  } else {
+    document.querySelector('#income')?.classList.add('hidden');
+    document.querySelector('#expense')?.classList.remove('hidden');
+  }
 }
-
-
